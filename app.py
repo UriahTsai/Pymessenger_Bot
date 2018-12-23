e6a036383654cf095054d0b57d68a359ea888a2b
Uriah Tsai <uriah1001@gmail.com>
2018-12-23 18:15:11 +0800

    Fixed Bugs

--- app.py
+++ app.py
@@ -24,16 +24,16 @@ def receive_message():
         for event in output['entry']:
             messaging = event['messaging']
             for message in messaging:
-            if message.get('message'):
+                if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
-                recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        response_sent_text = get_message()
+                        send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = get_message()
+                        send_message(recipient_id, response_sent_nontext)
 return "Message Processed"
 
 
