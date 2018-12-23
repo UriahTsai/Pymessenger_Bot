7d3f75c0e3f227a715638a2786cb6bc43ac9a4c3
Uriah Tsai <uriah1001@gmail.com>
2018-12-23 18:02:57 +0800

    Fixed Indention Error

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def receive_message():
         for event in output['entry']:
             messaging = event['messaging']
             for message in messaging:
-            if message.get('message'):
+                if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
